de64c5c0e45088c2aada30def1c3d91a70b67149
AKJAW <akjaworski1@gmail.com>
2023-05-05 14:21:26 +0200

    feat: add some OpenApi implementation

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,6 +35,7 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation("com.aallam.openai:openai-client:3.2.0")
             }
         }
         val androidMain by getting {
@@ -42,6 +43,7 @@ kotlin {
                 api("androidx.activity:activity-compose:1.6.1")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.9.0")
+                implementation("io.ktor:ktor-client-okhttp:2.3.0")
             }
         }
         val iosX64Main by getting
@@ -52,10 +54,14 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.0")
+            }
         }
         val desktopMain by getting {
             dependencies {
                 implementation(compose.desktop.common)
+                implementation("io.ktor:ktor-client-okhttp:2.3.0")
             }
         }
     }
--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -12,11 +12,30 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.aallam.openai.api.BetaOpenAI
+import com.aallam.openai.api.chat.ChatCompletion
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIConfig
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import kotlin.time.Duration.Companion.minutes
+
+val openAI = OpenAI(
+    OpenAIConfig(
+        token = OpenAiSecret.apiKey,
+        timeout = Timeout(request = 2.minutes, connect = 2.minutes, socket = 2.minutes)
+    )
+)
 
 @Composable
 fun App() {
+    val scope = rememberCoroutineScope()
     MaterialTheme {
         var userMessage by remember { mutableStateOf("") }
         val messages =
@@ -26,7 +45,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            LazyColumn(Modifier.weight(1f)) {
+            LazyColumn(Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 item {
                     Spacer(modifier = Modifier.height(8.dp))
                 }
@@ -48,9 +67,14 @@ fun App() {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Button(
                         onClick = {
-                            messages.add(userMessage)
-                            messages.add("Answer: ${userMessage.reversed()}")
-                            userMessage = ""
+                            scope.launch {
+                                val message = userMessage
+                                messages.add(message)
+                                userMessage = ""
+                                // TOOD when executing block input
+                                val answer = askChatGpt(message)
+                                messages.add("$answer")
+                            }
                         }
                     ) {
                         Text("Send")
@@ -65,6 +89,21 @@ fun App() {
             Spacer(modifier = Modifier.height(8.dp))
         }
     }
+}
+
+@OptIn(BetaOpenAI::class)
+private suspend fun askChatGpt(message: String): String? {
+    val chatCompletionRequest = ChatCompletionRequest(
+        model = ModelId("gpt-3.5-turbo"),
+        messages = listOf(
+            ChatMessage(
+                role = ChatRole.User,
+                content = message
+            )
+        )
+    )
+    val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
+    return completion.choices.first().message?.content
 }
 
 expect fun getPlatformName(): String
