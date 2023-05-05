bc47bc2cdcf9700d37d13bc97f24ecf2b13ce85b
AKJAW <akjaworski1@gmail.com>
2023-05-05 13:59:47 +0200

    feat: use LazyColumn for messages and add character count

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,48 +1,68 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
         var userMessage by remember { mutableStateOf("") }
         val messages =
-            remember { mutableStateListOf<String>("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et eros at nulla laoreet mollis. Donec a viverra turpis. Ut porta at turpis sit amet molestie. Morbi dignissim hendrerit lectus, ac cursus ante dapibus eu. Quisque lobortis metus quam, quis semper sapien interdum eget. Ut aliquam orci eget mauris tristique porttitor. Aliquam sit amet diam quis diam vestibulum egestas. Praesent rutrum erat at hendrerit euismod. Aenean volutpat sed urna bibendum molestie. Sed fringilla lacus viverra, viverra justo iaculis, dictum libero. Donec quis urna euismod, porttitor nulla vel, lobortis erat. Mauris ac nulla enim. Donec ultricies pharetra placerat. Pellentesque convallis luctus tristique.") }
+            remember { mutableStateListOf<String>() }
         Column(
             Modifier.fillMaxWidth().padding(horizontal = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Spacer(modifier = Modifier.height(8.dp))
-            messages.forEach { message ->
-                Card(modifier = Modifier.fillMaxWidth()) {
-                    Text(message, modifier = Modifier.fillMaxWidth().padding(8.dp))
+            LazyColumn(Modifier.weight(1f)) {
+                item {
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                itemsIndexed(items = messages, key = { index, _ -> index }) { index, message ->
+                    val background = if (index % 2 == 1) Color.LightGray else Color.White
+                    Card(modifier = Modifier.fillMaxWidth(), backgroundColor = background) {
+                        Text(message, modifier = Modifier.fillMaxWidth().padding(8.dp))
+                    }
                 }
             }
             Row(verticalAlignment = Alignment.CenterVertically) {
                 OutlinedTextField(
                     value = userMessage,
                     onValueChange = { userMessage = it },
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    maxLines = 10,
                 )
                 Spacer(modifier = Modifier.width(16.dp))
-                Button(
-                    onClick = {
-                        messages.add(userMessage)
-                        userMessage = ""
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Button(
+                        onClick = {
+                            messages.add(userMessage)
+                            messages.add("Answer: ${userMessage.reversed()}")
+                            userMessage = ""
+                        }
+                    ) {
+                        Text("Send")
                     }
-                ) {
-                    Text("Send")
+                    // TODO count tokens?
+                    Text(
+                        text = userMessage.count().toString(),
+                        style = MaterialTheme.typography.body2.copy(fontSize = 10.sp),
+                    )
                 }
             }
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
