a578939d77ec9be187ea83834ef8d7a0daa5ea9a
AKJAW <akjaworski1@gmail.com>
2023-06-01 16:11:44 +0200

    refactor: clean up composables

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +22,7 @@ import com.aallam.openai.api.http.Timeout
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
@@ -35,58 +37,67 @@ val openAI = OpenAI(
 
 @Composable
 fun App() {
-    val scope = rememberCoroutineScope()
     MaterialTheme {
-        var userMessage by remember { mutableStateOf("") }
-        val messages =
-            remember { mutableStateListOf<String>() }
-        Column(
-            Modifier.fillMaxWidth().padding(horizontal = 8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            LazyColumn(Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                item {
-                    Spacer(modifier = Modifier.height(8.dp))
-                }
-                itemsIndexed(items = messages, key = { index, _ -> index }) { index, message ->
-                    val background = if (index % 2 == 1) Color.LightGray else Color.White
-                    Card(modifier = Modifier.fillMaxWidth(), backgroundColor = background) {
-                        Text(message, modifier = Modifier.fillMaxWidth().padding(8.dp))
-                    }
+        ChatScreen()
+    }
+}
+
+@Composable
+private fun ChatScreen() {
+    var userMessage by remember { mutableStateOf("") }
+    val messages =
+        remember { mutableStateListOf<String>() }
+    // TODO extract screen
+    // TODO extract input?
+    // TODO add buttons at the top which changes the "Content"
+    Column(
+        Modifier.fillMaxWidth().padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        LazyColumn(Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            item {
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            itemsIndexed(items = messages, key = { index, _ -> index }) { index, message ->
+                val background = if (index % 2 == 1) Color.LightGray else Color.White
+                Card(modifier = Modifier.fillMaxWidth(), backgroundColor = background) {
+                    Text(message, modifier = Modifier.fillMaxWidth().padding(8.dp))
                 }
             }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                OutlinedTextField(
-                    value = userMessage,
-                    onValueChange = { userMessage = it },
-                    modifier = Modifier.weight(1f),
-                    maxLines = 10,
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Button(
-                        onClick = {
-                            scope.launch {
-                                val message = userMessage
-                                messages.add(message)
-                                userMessage = ""
-                                // TOOD when executing block input
-                                val answer = askChatGpt(message)
-                                messages.add("$answer")
-                            }
-                        }
-                    ) {
-                        Text("Send")
-                    }
-                    // TODO count tokens?
-                    Text(
-                        text = userMessage.count().toString(),
-                        style = MaterialTheme.typography.body2.copy(fontSize = 10.sp),
-                    )
-                }
+        }
+        ChatInput(
+            userMessage = userMessage,
+            setUserMessage = { userMessage = it },
+            onSend = { },
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Composable
+private fun ChatInput(
+    userMessage: String,
+    setUserMessage: (String) -> Unit,
+    onSend: () -> Unit,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        OutlinedTextField(
+            value = userMessage,
+            onValueChange = setUserMessage,
+            modifier = Modifier.weight(1f),
+            maxLines = 10,
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = onSend) {
+                Text("Send")
             }
-            Spacer(modifier = Modifier.height(8.dp))
+            // TODO count tokens?
+            Text(
+                text = userMessage.count().toString(),
+                style = MaterialTheme.typography.body2.copy(fontSize = 10.sp),
+            )
         }
     }
 }
