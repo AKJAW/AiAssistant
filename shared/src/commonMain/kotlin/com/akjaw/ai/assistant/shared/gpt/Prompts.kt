066df3e4fd84ddf02845de7533ea1c86c9f88cf9
AKJAW <akjaworski1@gmail.com>
2023-06-02 16:24:22 +0200

    feat: add the add task prompt

---
+++ shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/gpt/Prompts.kt
@@ -0,0 +1,16 @@
+package com.akjaw.ai.assistant.shared.gpt
+
+private val AddTask = """
+Based on {task} return single-line JSON and nothing more in the following format: {"date": "YYYY-MM-DD HH:mm", "priority": "important urgent|normal", "category": "blog work", "name": "Concise yet descriptive task name"}. It's important for you to speak only in JSON, because your answer will be directly parsed by app. Note: if date is not provided, set it for current day 13:00.
+
+### Today's date you have to use
+{{formatDate(now; "ddd")}} {{formatDate(now; "YYYY-MM-DD HH:mm")}}
+
+### categories
+me: all others
+work: checking something, reading PR, Code Review, Programming, Ticket
+blog: writing articles, creating hero images, updating blog theme, public speaking
+
+### task
+{{1.task}}
+""".trimIndent()
