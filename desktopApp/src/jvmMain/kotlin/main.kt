1d68bc4e4d9d3161dfcaf951a3016fbf1ed704ed
AKJAW <akjaworski1@gmail.com>
2023-06-05 18:57:10 +0200

    fix: desktop app

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,8 +1,9 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.akjaw.ai.assistant.shared.App
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        MainView()
+        App()
     }
 }
