f2711cca1d695c2c57bb8e6848bafa65bb79bb22
AKJAW <akjaworski1@gmail.com>
2023-07-09 10:24:18 +0200

    fix: ios kmp compilation

--- shared/src/iosMain/kotlin/com/akjaw/ai/assistant/shared/chat/data/database/DriverFactory.kt
+++ shared/src/iosMain/kotlin/com/akjaw/ai/assistant/shared/chat/data/database/DriverFactory.kt
@@ -1,6 +1,8 @@
 package com.akjaw.ai.assistant.shared.chat.data.database
 
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import com.akjaw.ai.assistant.database.Database
 
 actual class DriverFactory {
 
