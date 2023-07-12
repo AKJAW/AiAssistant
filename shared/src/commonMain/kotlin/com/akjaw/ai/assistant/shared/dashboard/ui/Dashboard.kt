4a70bb7b241c10053767c1f3efefbfa72028e3b2
AKJAW <akjaworski1@gmail.com>
2023-07-12 16:15:15 +0200

    feat: add version text in the top right

--- shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/dashboard/ui/Dashboard.kt
+++ shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/dashboard/ui/Dashboard.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,7 +28,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.akjaw.ai.assistant.shared.composition.Dependencies
@@ -35,6 +38,7 @@ import com.akjaw.ai.assistant.shared.chat.domain.ChatMessageHandler
 import com.akjaw.ai.assistant.shared.chat.presentation.ChatScreenStateHolder
 import com.akjaw.ai.assistant.shared.chat.ui.ChatScreen
 import com.akjaw.ai.assistant.shared.dashboard.domain.ChatType
+import com.akjaw.ai.assistant.shared.utils.BuildInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -99,17 +103,20 @@ fun DashboardScreen(initialType: ChatType?) {
     }
     Column(modifier = Modifier.padding(horizontal = 8.dp)) {
         Spacer(modifier = Modifier.height(4.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            ChatType.values().forEach { type ->
-                TypeButton(
-                    chatType = type,
-                    onClick = { dashboardScreenStateHolder.setType(type) },
-                    isSelected = dashboardScreenStateHolder.currentChatType == type
-                )
+        Box {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                ChatType.values().forEach { type ->
+                    TypeButton(
+                        chatType = type,
+                        onClick = { dashboardScreenStateHolder.setType(type) },
+                        isSelected = dashboardScreenStateHolder.currentChatType == type
+                    )
+                }
             }
+            Text(BuildInfo.version, modifier = Modifier.align(Alignment.TopEnd).alpha(0.3f))
         }
         Spacer(modifier = Modifier.height(4.dp))
         HorizontalPager(
--- shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/utils/BuildInfo.kt
+++ shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/utils/BuildInfo.kt
@@ -2,5 +2,6 @@ package com.akjaw.ai.assistant.shared.utils
 
 object BuildInfo {
 
+    val version: String = "1.0.1"
     var isDebug: Boolean = true
 }
