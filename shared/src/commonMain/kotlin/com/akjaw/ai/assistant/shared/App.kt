5332d5d72abc558e220e2ade58f8cd6a2e52bfe7
AKJAW <akjaworski1@gmail.com>
2023-07-12 16:03:27 +0200

    feat: clean up app coloring

--- shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/App.kt
+++ shared/src/commonMain/kotlin/com/akjaw/ai/assistant/shared/App.kt
@@ -5,10 +5,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.akjaw.ai.assistant.shared.dashboard.domain.ChatType
 import com.akjaw.ai.assistant.shared.dashboard.ui.DashboardScreen
+import com.akjaw.ai.assistant.shared.utils.BuildInfo
+
+val color = if (BuildInfo.isDebug) Color(0xFFC6433C) else Color(0xFF4E7DF8)
 
 @Composable
 fun App(initialType: ChatType? = null) {
-    MaterialTheme(colors = MaterialTheme.colors.copy(primary = Color(0xFF4E7DF8))) {
+    MaterialTheme(colors = MaterialTheme.colors.copy(primary = color)) {
         DashboardScreen(initialType)
     }
 }
