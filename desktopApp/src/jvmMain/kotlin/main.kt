6cab2ea5b9985af318cc8be7090e79d956fc3104
AKJAW <akjaworski1@gmail.com>
2023-07-14 15:22:25 +0200

    chore: add explicit desktop isDebug

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,3 +1,4 @@
+import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.akjaw.ai.assistant.shared.App
@@ -5,8 +6,10 @@ import com.akjaw.ai.assistant.shared.MainView
 import com.akjaw.ai.assistant.shared.composition.Dependencies
 import com.akjaw.ai.assistant.shared.chat.data.database.ProductionDriverFactory
 import com.akjaw.ai.assistant.shared.chat.data.database.createDatabase
+import com.akjaw.ai.assistant.shared.utils.BuildInfo
 
 fun main() = application {
+    BuildInfo.isDebug = true // For now change manually when creating a Jar
     createDatabase(ProductionDriverFactory())
     Window(onCloseRequest = ::exitApplication) {
         MainView()
